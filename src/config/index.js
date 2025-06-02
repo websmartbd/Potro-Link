73ebeafcaa5f6730805da90b5e1b854337ace94e
Web Smart BD <166507813+websmartbd@users.noreply.github.com>
2025-06-02 21:04:12 +0600

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   BOT_TOKEN: '7642795354:AAGxCU6-5RC8pgNRCLMQWa49bWKM-PdNsWs', // Replace with your actual bot token
   PORT: process.env.PORT || 80, // You can set a default port here
-  BASE_URL: process.env.BASE_URL || 'http://localhost', // Base URL for the application
+  BASE_URL: process.env.BASE_URL || 'https://potro.onrender.com', // Base URL for the application
 };
 
 module.exports = config; 
