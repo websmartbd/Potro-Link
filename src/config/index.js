d3cae16fc4f60efc2f79437ed17c122cda74a972
Web Smart BD <166507813+websmartbd@users.noreply.github.com>
2025-06-02 20:59:05 +0600

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const config = {
   PORT: process.env.PORT || 80, // You can set a default port here
   BASE_URL:
     process.env.BASE_URL ||
-    "https://fa1cd4b1-a7a1-4c1e-ac23-0d0462ad5ace-00-3a0lrn128t7sn.pike.replit.dev/", // Base URL for the application
+    "https://potro.onrender.com", // Base URL for the application
 };
 
 module.exports = config;
