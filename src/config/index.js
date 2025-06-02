fd5946fa3b559063a11ead538faa4b3a3e3e7065
Web Smart BD <166507813+websmartbd@users.noreply.github.com>
2025-06-02 20:46:09 +0600

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,9 @@
 const config = {
-  BOT_TOKEN: '7642795354:AAGxCU6-5RC8pgNRCLMQWa49bWKM-PdNsWs', // Replace with your actual bot token
-  PORT: process.env.PORT ||80, // You can set a default port here
+  BOT_TOKEN: "7642795354:AAGxCU6-5RC8pgNRCLMQWa49bWKM-PdNsWs", // Replace with your actual bot token
+  PORT: process.env.PORT || 80, // You can set a default port here
+  BASE_URL:
+    process.env.BASE_URL ||
+    "https://fa1cd4b1-a7a1-4c1e-ac23-0d0462ad5ace-00-3a0lrn128t7sn.pike.replit.dev/", // Base URL for the application
 };
 
-module.exports = config; 
+module.exports = config;
